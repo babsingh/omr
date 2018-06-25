1e52ac60d13650d95771c8d3fe52c63060c6d93e
Babneet Singh <sbabneet@ca.ibm.com>
2018-06-25 15:32:59 -0400

    Use omrsig_primary_signal instead of OMRSIG_SIGNAL
    Signed-off-by: Babneet Singh <sbabneet@ca.ibm.com>

--- port/win64amd/omrsignal.c
+++ port/win64amd/omrsignal.c
@@ -28,9 +28,7 @@
 #include "omrthread.h"
 #include "ut_omrport.h"
 
-#if defined(OMRPORT_OMRSIG_SUPPORT)
 #include "omrsig.h"
-#endif /* defined(OMRPORT_OMRSIG_SUPPORT) */
 
 #include "omrutil.h"
 
@@ -1456,7 +1454,7 @@ sig_full_shutdown(struct OMRPortLibrary *portLibrary)
 		/* Register the original signal handlers, which were overwritten. */
 		for (index = 1; index < ARRAY_SIZE_SIGNALS; index++) {
 			if (handlerInfo[index].restore) {
-				OMRSIG_SIGNAL(index, handlerInfo[index].originalHandler);
+				omrsig_primary_signal(index, handlerInfo[index].originalHandler);
 				/* record that we no longer have a handler installed with the OS for this signal */
 				Trc_PRT_signal_sig_full_shutdown_deregistered_handler_with_OS(portLibrary, index);
 				handlerInfo[index].restore = 0;
@@ -1607,7 +1605,7 @@ registerSignalHandlerWithOS(OMRPortLibrary *portLibrary, uint32_t portLibrarySig
         return OMRPORT_SIG_ERROR;
     }
 
-    localOldOSHandler = OMRSIG_SIGNAL(osSignalNo, handler);
+    localOldOSHandler = omrsig_primary_signal(osSignalNo, handler);
     if (SIG_ERR == localOldOSHandler) {
         Trc_PRT_signal_registerSignalHandlerWithOS_failed_to_registerHandler(portLibrarySignalNo, osSignalNo, handler);
         return OMRPORT_SIG_ERROR;
@@ -1657,7 +1655,7 @@ masterASynchSignalHandler(int osSignalNo)
 	/* Signal handler is reset after invocation. So, the signal handler needs to
 	 * be registered again after it is invoked.
 	 */
-	OMRSIG_SIGNAL(osSignalNo, masterASynchSignalHandler);
+	omrsig_primary_signal(osSignalNo, masterASynchSignalHandler);
 }
 
 /**
